271bcc3935dbd859c8f61142c3a24dbb2733e3ad
alpaslan-53 <79101461+alpaslan-53@users.noreply.github.com>
2023-03-30 17:04:41 +0300

    CodeFirst temel yaklaşım

---
+++ CodeFirst/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello, World!");
+
+
+public class DenemeDbContext :DbContext
+{
+   public DbSet<Urun> Urunler { get; set; }
+   public DbSet<Musteri> Musteriler { get; set; }
+    public DbSet<Siparis> Siparisler { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+       optionsBuilder.UseSqlServer("Server=DESKTOP-E30TBPJ;Database=CodeFirst;Trusted_Connection=True;TrustServerCertificate=Yes"
+           );
+    }
+}
+public class Urun
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+    public double Price { get; set; }
+}
+
+public class Musteri
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string Gender { get; set; }
+} 
